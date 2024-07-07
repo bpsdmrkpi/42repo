5102aa465a39410d71eae420e2901c1f3f7c0d84
bpsdmrkpi <bpasademirkapi@gmail.com>
2024-07-07 12:48:27 +0300

    Update ft_print_comb.c düzeltildi

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -1,39 +1,40 @@
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void    ft_print_comb(void)
 {
-	write(1, &c, 1);
-}
+        int a;
+        int b;
+        int c;
 
-void	ft_print_comb(void)
-{
-	int a;
-	int b;
-	int c;
+        a = '0';
 
-	a = '0';
-
-	while(a <= '7')
-	{
-		b = a + 1;
-		while(b <= '8')
-		{
-			c = b + 1;
-			while(c <= '9')
-			{
-				write (1, &a, 1);
-				write (1, &b, 1);
-				write (1, &c, 1);
-				write (1, ", ", 2);
-				c++;
-			}
-			b++;
-		}
-		a++;
-	}
+        while(a <= '7')
+        {
+                b = a + 1;
+                while(b <= '8')
+                {
+                        c = b + 1;
+                        while(c <= '9')
+                        {
+                                write (1, &a, 1);
+                                write (1, &b, 1);
+                                write (1, &c, 1);
+                                if(a != '7' || b != '8' || c != '9')
+                                {
+                                        write (1, ", ", 2);
+                                }
+                                c++;
+                        }
+                        b++;
+                }
+                a++;
+        }
 }
 
 void main(void)
+{
+        ft_print_comb();
+}
 {
 	ft_print_comb();
 }
